75846c3ba9ddf2114bb35d22433fbf8d5732e08b
mjun413 <48771142+mjun413@users.noreply.github.com>
2019-11-14 13:13:35 -0500

    Update powercalculator.js

--- powercalculator.js
+++ powercalculator.js
@@ -1,1 +1,15 @@
+const powerCalculator = (num, power) => {
+  //check if greater than or equal to zero
+  if (power < 0) {
+    return `exponent should be >= 0`;
+  }
+  //base case
+  if (power === 0) {
+    return 1;
+  }
 
+  //general case
+  return num * powerCalculator(num, power - 1);
+};
+
+console.log(powerCalculator(10, 2));
