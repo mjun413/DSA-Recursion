9e5a880bed3adee7c0919bb9f22f442d077768d1
mjun413 <48771142+mjun413@users.noreply.github.com>
2019-11-14 12:54:57 -0500

    Update coutingsheep.js

--- coutingsheep.js
+++ coutingsheep.js
@@ -1,1 +1,10 @@
+function countingSheep(num) {
+  if (num === 0) {
+    return "All sheep jumped over the fence";
+  } else (
+    return num + ": Another sheep jumps over the fence";
+    num = num - 1;
+  )
+}
 
+countingSheep(3)
